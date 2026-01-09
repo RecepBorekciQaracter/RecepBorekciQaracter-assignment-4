0c68165f873c79334ec2c3b6e56b0f7d9f78eafc
Recep Borekci <recep.borekci@qaracter.com>
2026-01-09 14:36:28 +0300

    Initial commit - Springboot Project Initialization

---
+++ src/main/java/com/example/RecepBorekciQaracter_assignment_4/RecepBorekciQaracterAssignment4Application.java
@@ -0,0 +1,13 @@
+package com.example.RecepBorekciQaracter_assignment_4;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RecepBorekciQaracterAssignment4Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RecepBorekciQaracterAssignment4Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/RecepBorekciQaracter_assignment_4/RecepBorekciQaracterAssignment4ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.RecepBorekciQaracter_assignment_4;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RecepBorekciQaracterAssignment4ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
